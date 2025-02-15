282d817854afb2ce7967665525877de18c827d00
TomasAlric <tomas.alricc@gmail.com>
2025-02-15 17:43:52 -0300

    fix: base url

--- index/script.js
+++ index/script.js
@@ -1,5 +1,5 @@
 // Defina a variável com o IP do servidor
-const API_BASE_URL = "http://localhost:5000"; // Altere aqui para mudar o IP
+const API_BASE_URL = "http://127.0.0.1:5000"; // Altere aqui para mudar o IP
 
 // Função para listar os alunos
 function listarAlunos() {
